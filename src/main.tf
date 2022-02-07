58bc6c96fc5178e98d4428e07f3ce531bfdb5745
uolter <walter.traspadini@pagopa.it>
2022-02-07 18:44:52 +0100

    backend arn role

--- src/main.tf
+++ src/main.tf
@@ -8,28 +8,28 @@ terraform {
 
   required_version = ">= 1.1.5"
 
-   backend "s3" {
-    bucket         = "terraform-backend-11670"
+  backend "s3" {
+    bucket         = "terraform-backend-11048"
     key            = "tfstate"
     region         = "us-west-2"
     encrypt        = true
     dynamodb_table = "terraform-lock"
-    # role_arn       = "arn:aws:iam::<AWS_ACCOUNT_ID_OF_BACKEND>:role/terraform-backend"
+    role_arn       = "arn:aws:iam::476761402556:role/terraform-backend"
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_vpc" "example" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = "true" #gives you an internal domain name
-    enable_dns_hostnames = "true" #gives you an internal host name
-    enable_classiclink = "false"
-    instance_tenancy = "default"    
-    
-    tags = {
-        Name = "Example"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true" #gives you an internal domain name
+  enable_dns_hostnames = "true" #gives you an internal host name
+  enable_classiclink   = "false"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "Example"
+  }
 }
